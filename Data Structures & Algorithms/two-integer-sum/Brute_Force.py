2429fd9dadcb220abbc660c6587d2bbcd149a416
avezkharni92 <avezkharni@gmail.com>
2026-07-05 16:29:58 +0530

    Added comments

--- Data Structures & Algorithms/two-integer-sum/Brute_Force.py
+++ Data Structures & Algorithms/two-integer-sum/Brute_Force.py
@@ -5,4 +5,20 @@ class Solution:
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
+'''1
+. Brute Force
+Intuition
+We can check every pair of different elements in the array and return the first pair that sums up to the target. This is the most intuitive approach but it's not the most efficient.
+
+Algorithm
+Iterate through the array with two nested loops using indices i and j to check every pair of different elements.
+If the sum of the pair equals the target, return the indices of the pair.
+If no such pair is found, return an empty array.
+There is guaranteed to be exactly one solution, so we will never return an empty array.
+Time & Space Complexity
+Time complexity: 
+O(n2)
+Space complexity: 
+O(1)
+'''
         
