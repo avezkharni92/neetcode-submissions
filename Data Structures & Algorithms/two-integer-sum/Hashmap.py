b0ea0b6d29827a7ea4b1fec5e7607c06487e2673
avezkharni92 <avezkharni@gmail.com>
2026-07-05 16:34:00 +0530

    Improve comment clarity in twoSum method
    Clarified comment in the for loop to explain the index and value usage.

--- Data Structures & Algorithms/two-integer-sum/Hashmap.py
+++ Data Structures & Algorithms/two-integer-sum/Hashmap.py
@@ -1,7 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashmap = {} #store value and index format
-        for i, v in enumerate(nums):  #But in for loop you use index and value
+        for i, v in enumerate(nums):  #But in for loop you use index and value which is reverse of above value
             diff = target - v
             if diff in hashmap:
                 return [hashmap[diff], i]
