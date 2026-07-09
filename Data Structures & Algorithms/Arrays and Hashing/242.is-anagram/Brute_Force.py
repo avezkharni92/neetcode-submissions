b45c966441a5e0a9562519473485d2f41e781dc6
avezkharni92 <avezkharni@gmail.com>
2026-07-09 19:27:42 +0530

    CHANGED

--- Data Structures & Algorithms/Arrays and Hashing/242.is-anagram/Brute_Force.py
+++ Data Structures & Algorithms/Arrays and Hashing/242.is-anagram/Brute_Force.py
@@ -3,4 +3,46 @@ class Solution:
         if len(s) != len(t):
             return False
         return sorted(s) == sorted(t) #sorted can be used in return only
+    
+# 1. Sorting
+# Intuition
+# If two strings are anagrams, they must contain exactly the same characters with the same frequencies.
+# By sorting both strings, all characters will be arranged in a consistent order.
+# If the two sorted strings are identical, then every character and its count match, which means the strings are anagrams.
+
+# Algorithm
+# If the lengths of the two strings differ, return false immediately because they cannot be anagrams.
+# Sort both strings.
+# Compare the sorted versions of the strings:
+# If they are equal, return true.
+# Otherwise, return false.
+
+# Time & Space Complexity
+# Time complexity: 
+# O
+# (
+# n
+# log
+# ⁡
+# n
+# +
+# m
+# log
+# ⁡
+# m
+# )
+# O(nlogn+mlogm)
+# Space complexity: 
+# O
+# (
+# 1
+# )
+# O(1) or 
+# O
+# (
+# n
+# +
+# m
+# )
+# O(n+m) depending on the sorting algorithm.
         
--- Data Structures & Algorithms/Arrays and Hashing/242.is-anagram/HashTable.py
+++ Data Structures & Algorithms/Arrays and Hashing/242.is-anagram/HashTable.py
@@ -2,14 +2,16 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        #here we are using hashmap
-        #initiate the empty hashmap/dictionary for both the strings
-        countS, countT = {}, {}
-        #use the get function of dictionary to give a value of a key
+
+        count = [0] * 26
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0) #get function check for the value of a key in dictionary. here key is the character
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        return countS == countT
+            count[ord(s[i]) - ord('a')] += 1
+            count[ord(t[i]) - ord('a')] -= 1
+
+        for val in count:
+            if val != 0:
+                return False
+        return True
     
 # 3. Hash Table (Using Array)
 # Intuition
